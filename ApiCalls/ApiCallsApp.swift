70849c07f1b9ccaa1a02a0e599ac3d2c943cdb09
LukasSkrivanek <luky.skrivos@gmail.com>
2024-07-31 14:23:31 +0200

    Initial Commit

---
+++ ApiCalls/ApiCallsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiCallsApp.swift
+//  ApiCalls
+//
+//  Created by macbook on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ApiCallsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
